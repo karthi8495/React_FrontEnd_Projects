7171737de6f02ab63862621efdace95c7d047fb1
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-10-03 01:23:22 +0530

    New Project Added

--- realtime_project/src/App.jsx
+++ realtime_project/src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
- 
-
   return (
     <div>
       {/* <Accordian /> */}
 
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      {/* <StarRating  noOfStars={10}/> */}
+
+      {/* <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> */}
+
+
+
     </div>
   );
 }
 
-export default App
+export default App;
