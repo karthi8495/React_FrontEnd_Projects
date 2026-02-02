8bc92f7c3e0241b7fc9aed847af83c767deb05f6
karthikeyan <karthikeyanmadhappan@gmail.com>
2026-02-02 22:42:12 +0530

    update

--- realtime_project/src/App.jsx
+++ realtime_project/src/App.jsx
@@ -1,16 +1,23 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import "./App.css";
-import Accordian from "./components/accordian";
-import RandomColor from "./components/random-color";
-import StarRating from "./components/star-rating";
-import ImageSlider from "./components/image-slider";
-import LoadMoreData from "./components/load-more-data";
-import MenuView from "./components/menu-view";
-// import menus from "./components/menu-view";
-import QRCodeGenerator from "./components/qr-code-generator";
-import LightDarkMode from "./components/light-dark-mode";
+// import "./App.css";
+// import Accordian from "./components/accordian";
+// import RandomColor from "./components/random-color";
+// import StarRating from "./components/star-rating";
+// import ImageSlider from "./components/image-slider";
+// import LoadMoreData from "./components/load-more-data";
+// import MenuView from "./components/menu-view";
+// // import menus from "./components/menu-view";
+// import QRCodeGenerator from "./components/qr-code-generator";
+// import LightDarkMode from "./components/light-dark-mode";
+// import ScrollIndicator from "./components/scroll-indicator";
+
+import ModalTest from "./components/custom-modal-popup/modal-test";
+
+
+
+// import TabTest from "./components/custom-tabs/tab-test";
 
 
 function App() {
@@ -34,7 +41,25 @@ function App() {
 
       {/* <QRCodeGenerator /> */}
 
-      <LightDarkMode />
+
+      {/* This is error for use local storage doent fixed */}
+      {/* <LightDarkMode /> */}
+
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
+
+
+      {/* <TabTest /> */}
+
+      <ModalTest />
+
+
+
+
+
+
+
+
+
 
     </div>
   );
